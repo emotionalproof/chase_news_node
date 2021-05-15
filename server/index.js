6ded6190e3be5c2a6dabc5761d39073f97aa0cb8
emotionalproof <chaserabenn@gmail.com>
2021-05-15 09:31:51 -0400

    update

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
-const express = require("express")
-const PORT = process.env.PORT || 3001
+const express = require("express");
+const PORT = process.env.PORT || 3001;
 
-const app = express()
+//enable CORS
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.options("*", cors());
+
+app.get("/", (req, res) => {
+	res.json({ message: "Welcome" });
+});
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" })
-})
+	res.json({ message: "Hello from server!" });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
-})
+	console.log(`Server listening on ${PORT}`);
+});
